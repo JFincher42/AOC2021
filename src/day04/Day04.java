c2cebe093722ba5fda71c6fa0beae2a27e69dd59
Jon Fincher <jfincher42@gmail.com>
2021-12-06 08:17:45 -0600

    Day04 cleanup

--- src/day04/Day04.java
+++ src/day04/Day04.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 
 class BingoCard{
 	int[][] card;
+	public boolean won;
 	
 	BingoCard(String[] lines){
 		this.card = new int[5][5];
@@ -17,6 +18,7 @@ class BingoCard{
 				this.card[i][j] = Integer.parseInt(entries[j]);
 			}
 		}
+		this.won = false;
 	}
 	
 	void markNumber(int number) {
@@ -34,18 +36,18 @@ class BingoCard{
 				(card[i][1]==-1) &&
 				(card[i][2]==-1) &&
 				(card[i][3]==-1) &&
-				(card[i][4]==-1))	return true;
+				(card[i][4]==-1))	this.won = true;
 			
 			// Is there a column bingo?
 			if ((card[0][i]==-1) && 
 				(card[1][i]==-1) &&
 				(card[2][i]==-1) &&
 				(card[3][i]==-1) &&
-				(card[4][i]==-1))	return true;
+				(card[4][i]==-1))	this.won = true;
 				
 		}
 		// No bingo anywhere
-		return false;
+		return this.won;
 	}
 
 	int getSumRemaining() {
@@ -98,13 +100,56 @@ public class Day04 {
 	}
 	
 	public static int part2(ArrayList<String> lines) {
+		BingoCard lastCard=null; 
+
+		// The first line is our called numbers
+		getCalledNumbers(lines.get(0));
+		
+		// Skip the blank, and start processing bingo cards
+		int currentLine = 1;
+		while (currentLine < lines.size()-1) {
+			String[] cardLines = (String[])lines.subList(currentLine, currentLine+5).toArray(new String[0]);
+			bingoCards.add(new BingoCard(cardLines));
+			// Get past this card and the blank line
+			currentLine += 5;
+		}
+		
+		for (int call:calledNumbers) {
+			// Mark all the numbers
+			for (BingoCard card: bingoCards) {
+				card.markNumber(call);
+				card.hasBingo();
+			}
+			// How many bingo cards are left?
+			int bingoCount = 0;
+			for (BingoCard card: bingoCards) {
+				if (!card.won) {
+					bingoCount += 1;
+					lastCard = card;
+				}
+			}
+			// Is there only one? Let's get out and just run that one
+			if (bingoCount == 1) break;
+			
+		}
+		// lastCard should be the final bingo card, so let's play it
+		// until it wins.
+		// We can restart the called numbers, since they're all
+		// marked by this point anyway
+		for (int call:calledNumbers) {
+			lastCard.markNumber(call);
+			if (lastCard.hasBingo()) {
+				System.out.println("Card: " + call + ", bingo sum: " + lastCard.getSumRemaining());
+				return call*lastCard.getSumRemaining();
+			}		
+		}
 		return 0;
 	}
 	
 	public static void main(String[] argv) {
 		BufferedReader input = null;
-//		String filename="src/day04/input";
-		String filename="src/day04/sample";
+		String filename="src/day04/input";
+//		String filename="src/day04/sample";
 		
 		ArrayList<String> lines = new ArrayList<>();
 		
@@ -122,7 +167,7 @@ public class Day04 {
 			System.exit(1);
 		}
 
-		System.out.println("Part 1: " + part1(lines));
+//		System.out.println("Part 1: " + part1(lines));
 		System.out.println("Part 2: " + part2(lines));
 
 	}
