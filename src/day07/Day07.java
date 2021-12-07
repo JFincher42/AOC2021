5add21af35626afad6a7d803024bffce4d419701
Jon Fincher <jfincher42@gmail.com>
2021-12-07 16:46:25 -0600

    Day 07 complete and cleaned up

--- src/day07/Day07.java
+++ src/day07/Day07.java
@@ -4,20 +4,58 @@ package day07;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Day07 {
 	
-	public static int part1(ArrayList<String> lines) {
-		return 0;
+	public static int calcCost2(int position, int[] posList) {
+		int cost = 0;
+		for (int i = 0; i<posList.length; i++) {
+			int n = Math.abs(position - posList[i]);
+			cost += ((n+1) * n) / 2;
+		}
+		return cost;
 	}
 	
-	public static int part2(ArrayList<String> lines) {
-		return 0;
+	public static int calcCost(int position, int[] posList) {
+		int cost = 0;
+		for (int i = 0; i<posList.length; i++)
+			cost += Math.abs(posList[i] - position);
+		return cost;
+	}
+	
+	public static int part1(int[] pos) {		
+		// Find the biggest and smallest
+		int min = IntStream.of(pos).min().getAsInt();
+		int max = IntStream.of(pos).max().getAsInt();
+		int cost = Integer.MAX_VALUE;
+		
+		for (int i=min; i<=max; i++) {
+			int newCost = calcCost(i, pos);
+			cost = Math.min(cost,  newCost);
+		}
+		return cost;
+	}
+	
+	public static int part2(int[] pos) {
+		// Find the biggest and smallest
+		int min = IntStream.of(pos).min().getAsInt();
+		int max = IntStream.of(pos).max().getAsInt();
+		int cost = Integer.MAX_VALUE;
+		
+		for (int i=min; i<=max; i++) {
+			int newCost = calcCost2(i, pos);
+			cost = Math.min(cost,  newCost);
+		}
+		return cost;
 	}
 	
 	public static void main(String[] argv) {
 		BufferedReader input = null;
 		String filename="src/day07/input";
+//		String filename="src/day07/sample";
 		
 		ArrayList<String> lines = new ArrayList<>();
 		
@@ -35,8 +73,16 @@ public class Day07 {
 			System.exit(1);
 		}
 
-		System.out.println("Part 1: " + part1(lines));
-		System.out.println("Part 2: " + part2(lines));
+		// Convert to an array of ints
+		String[] strPos = lines.get(0).split(",");
+		
+		int[] pos = new int[strPos.length];
+		for (int i=0; i<strPos.length; i++)
+			pos[i] = Integer.parseInt(strPos[i]);
+		
+		// Get the answers
+		System.out.println("Part 1: " + part1(pos));
+		System.out.println("Part 2: " + part2(pos));
 
 	}
 }
