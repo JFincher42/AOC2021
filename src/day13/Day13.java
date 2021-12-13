450620544a6886763d229565cd1f0e603f966a8e
Jon Fincher <jfincher42@gmail.com>
2021-12-12 20:38:27 -0600

    Day 13 setup

---
+++ src/day13/Day13.java
@@ -0,0 +1,42 @@
+package day13;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class Day13 {
+	
+	public static int part1(ArrayList<String> lines) {
+		return 0;
+	}
+	
+	public static int part2(ArrayList<String> lines) {
+		return 0;
+	}
+	
+	public static void main(String[] argv) {
+		BufferedReader input = null;
+//		String filename="src/day13/input";
+		String filename="src/day13/sample";
+		
+		ArrayList<String> lines = new ArrayList<>();
+		
+		try {
+			// Open the input file
+			input = new BufferedReader(new FileReader(filename));
+			
+			// Read every line
+			String line;
+			while ((line = input.readLine()) != null)
+				if (line.length()>0)
+					lines.add(line);
+		} catch (Exception e) {
+			System.out.println("Problem reading input");
+			System.exit(1);
+		}
+
+		System.out.println("Part 1: " + part1(lines));
+		System.out.println("Part 2: " + part2(lines));
+
+	}
+}
