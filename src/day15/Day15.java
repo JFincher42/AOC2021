d4e08dec2fae181addf0cb3e79f1484f2a0b34cf
Jon Fincher <jfincher42@gmail.com>
2021-12-15 14:42:10 -0600

    Removed some useless stuff

--- src/day15/Day15.java
+++ src/day15/Day15.java
@@ -6,47 +6,44 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 
 public class Day15 {
-	ArrayList<Integer> pathLengths = new ArrayList<>();
-
 	
-	public static long findPath(int[][] floor) {
+	public static int findPath(int[][] floor) {
 		// Get the length of the floor
 		int endR = floor.length, endC = floor[0].length;
 		
 		// To hold the shortest path to every node
-		long[][] shortest = new long[endR][endC];
+		int[][] shortest = new int[endR][endC];
 		// Initialize it to all -1
 		for (int r=0; r<shortest.length; r++)
 			for (int c=0; c<shortest[r].length; c++)
-				shortest[r][c] = -1l;
+				shortest[r][c] = -1;
 		
 		// To hold the next nodes to visit
 		// We use an int[] to represent a tuple of three values
 		// r and c are the coordinates of the node to visit
 		// length is the length of the path before we entered.
-		ArrayDeque<Long[]> nodes = new ArrayDeque<>();
+		ArrayDeque<Integer[]> nodes = new ArrayDeque<>();
 		
 		// Start at 0
-		shortest[0][0] = 0l;
+		shortest[0][0] = 0;
 		
 		// Push the next two nodes onto the queue
-		nodes.add(new Long[] {0l, 1l, 0l});
-		nodes.add(new Long[] {1l, 0l, 0l});
-		shortest[0][1] = Long.MAX_VALUE;
-		shortest[1][0] = Long.MAX_VALUE;
+		nodes.add(new Integer[] {0, 1, 0});
+		nodes.add(new Integer[] {1, 0, 0});
+		shortest[0][1] = Integer.MAX_VALUE;
+		shortest[1][0] = Integer.MAX_VALUE;
 		
 		// While we still have nodes to visit
 		while (!nodes.isEmpty()) {
 			// Get the next node from the queue, decode the contents
-			Long[] node = nodes.remove();
-			long rl = node[0]; long cl = node[1]; long l = node[2];
-			int r = (int)rl; int c = (int)cl;
+			Integer[] node = nodes.remove();
+			int r = node[0]; int c = node[1]; int l = node[2];
 			
 			// Where are we?
 //			System.out.println("Visiting node["+ r + "]["+ c + "], length=" + l + ", nodes remaining=" + nodes.size());
 
 			// Is this the first time visiting this node?
-			if (shortest[r][c] == Long.MAX_VALUE)
+			if (shortest[r][c] == Integer.MAX_VALUE)
 				// Set the shortest path
 				shortest[r][c] = l + floor[r][c];
 			else
@@ -59,56 +56,56 @@ public class Day15 {
 			if (c-1 >= 0) {
 				// If the top node hasn't been visited, visit it
 				if (shortest[r][c-1] == -1) {
-					nodes.add(new Long[] {(long)r, (long)c-1, shortest[r][c]});
-					shortest[r][c-1] = Long.MAX_VALUE;
+					nodes.add(new Integer[] {r, c-1, shortest[r][c]});
+					shortest[r][c-1] = Integer.MAX_VALUE;
 				}
 				// Else if we have a shorter path, set that path
 				else
 					if (shortest[r][c] + floor[r][c-1] < shortest[r][c-1]) {
 					shortest[r][c-1] = shortest[r][c] + floor[r][c-1];
-					nodes.add(new Long[] {(long)r, (long)c-1, shortest[r][c]});
+					nodes.add(new Integer[] {r, c-1, shortest[r][c]});
 				}
 			}
 			
 			if (c+1 < endC) {
 				// Node below us
 				if (shortest[r][c+1] == -1) { 
-					nodes.add(new Long[] {(long)r, (long)c+1, shortest[r][c]});
-					shortest[r][c+1] = Long.MAX_VALUE;
+					nodes.add(new Integer[] {r, c+1, shortest[r][c]});
+					shortest[r][c+1] = Integer.MAX_VALUE;
 				}
 				// Else if we have a shorter path, set that path
 				else 
 					if (shortest[r][c] + floor[r][c+1] < shortest[r][c+1]) {
 					shortest[r][c+1] = shortest[r][c] + floor[r][c+1];
-					nodes.add(new Long[] {(long)r, (long)c+1, shortest[r][c]});
+					nodes.add(new Integer[] {r, c+1, shortest[r][c]});
 				}
 			}
 			
 			if (r-1 >= 0) {
 				// If the left node hasn't been visited, or has a longer path
 				if (shortest[r-1][c] == -1) {
-					nodes.add(new Long[] {(long)r-1, (long)c, shortest[r][c]});
-					shortest[r-1][c] = Long.MAX_VALUE;
+					nodes.add(new Integer[] {r-1, c, shortest[r][c]});
+					shortest[r-1][c] = Integer.MAX_VALUE;
 				}
 				// Else if we have a shorter path, set that path
 				else 
 					if (shortest[r][c] + floor[r-1][c] < shortest[r-1][c]) {
 					shortest[r-1][c] = shortest[r][c] + floor[r-1][c];
-					nodes.add(new Long[] {(long)r-1, (long)c, shortest[r][c]});
+					nodes.add(new Integer[] {r-1, c, shortest[r][c]});
 				}
 			}
 			
 			if (r+1 < endR) {
 				// Node to the right of us
 				if (shortest[r+1][c] == -1) {
-					nodes.add(new Long[] {(long)r+1, (long)c, shortest[r][c]});
-					shortest[r+1][c] = Long.MAX_VALUE;
+					nodes.add(new Integer[] {r+1, c, shortest[r][c]});
+					shortest[r+1][c] = Integer.MAX_VALUE;
 				}
 				// Else if we have a shorter path, set that path
 				else 
 					if (shortest[r][c] + floor[r+1][c] < shortest[r+1][c]) {
 					shortest[r+1][c] = shortest[r][c] + floor[r+1][c];
-					nodes.add(new Long[] {(long)r+1, (long)c, shortest[r][c]});
+					nodes.add(new Integer[] {r+1, c, shortest[r][c]});
 				}
 			}
 		}
@@ -117,12 +114,12 @@ public class Day15 {
 		
 	}
 	
-	public static long part1(int[][] floor) {
+	public static int part1(int[][] floor) {
 				
 		return findPath(floor);
 	}
 	
-	public static long part2(int[][] floor) {
+	public static int part2(int[][] floor) {
 
 		// Make the new larger floor
 		int rMax = floor.length;
